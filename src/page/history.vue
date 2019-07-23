b9cecef211b1d7b02c3a161ba9812458828f6c41
lovecrossyou <lovecrossyoutt@gmail.com>
2019-07-23 18:03:50 +0800

    xx

--- src/page/history.vue
+++ src/page/history.vue
@@ -2,33 +2,67 @@
   <div class="container">
     <div class="content">
       <head-top textColor="#fff"></head-top>
+
+      <map :center="center" :zoom="7" class="map">
+        <marker
+          v-for="m in markers"
+          :position.sync="m.position"
+          :clickable="true"
+          :draggable="true"
+          @g-click="center=m.position"
+          :key="m.id"
+        ></marker>
+      </map>
     </div>
   </div>
 </template>
 
 <script>
 import headTop from "@/components/headTop";
-import { mapState } from 'vuex';
+import { mapState } from "vuex";
+import { load, Map, Marker } from "vue-google-maps";
+load("AIzaSyB59tFRny_neBuSc0WWh82a-QtgXkYyetg", "");
 
 export default {
   components: {
-    headTop
+    headTop,
+    Map,
+    Marker
   },
-  computed:{
-    ...mapState('shop',['shoplist'])
+  computed: {
+    ...mapState("shop", ["shoplist"])
+  },
+  data() {
+    return {
+      center: { lat: 10.0, lng: 10.0 },
+      markers: [
+        {
+          id:'001',
+          position: { lat: 10.0, lng: 10.0 }
+        },
+        {
+           id:'002',
+          position: { lat: 11.0, lng: 11.0 }
+        }
+      ]
+    };
   },
   methods: {
     goNext() {
       this.$router.push("home");
     }
   },
-  created(){
-    console.log('shoplist ', this.shoplist);
-  }
+  created() {
+    console.log("shoplist ", this.shoplist);
+  },
 };
 </script>
 
 <style  scoped>
+.map{
+  width: 600px;
+  height: 600px;
+}
 .container {
   width: 100%;
   display: flex;
@@ -44,5 +78,4 @@ export default {
   box-sizing: border-box;
   position: relative;
 }
-
 </style>
